3f23264650a33a70b401eefe5deb4e51203e7eb0
user <user@DESKTOP-ESV96G5>
2023-05-12 12:23:28 +0900

    230512 hotleList일부수정 Merge branch 'jayk' of https://github.com/ByungGwan27/Midterm-project.git into developerjayk

--- src/main/java/jayk/hotelinfo/control/AdminAddRoomControl.java
+++ src/main/java/jayk/hotelinfo/control/AdminAddRoomControl.java
@@ -85,10 +85,11 @@ public class AdminAddRoomControl implements Control {
 		HotelInfoService service = new HotelInfoServiceImpl();
 				
 		if(service.addAdminRoom(addRoom) && service.addAdminRoomImage(addRoom) && service.addAdminRoomDetail(addRoom)) {
-			//System.out.println("성공");
+			System.out.println("성공");
 			return "adminHotelList.do";
 		}else {
-			return "adminAddHotelPage.do";
+			System.out.println("실패");
+			return "adminHotelList.do";
 		}
 	}
 
